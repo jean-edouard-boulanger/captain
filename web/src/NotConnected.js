f8dd575c9afde02ce3291b486402e794252505ed
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2021-06-13 10:45:44 +0200

    actions menu sections

--- web/src/NotConnected.js
+++ web/src/NotConnected.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import {
   Card,
+  Grid,
   CardContent,
   Typography,
 } from "@material-ui/core";
+import PortableWifiOffIcon from '@material-ui/icons/PortableWifiOff';
 
 
 export function NotConnected({connection}) {
   return (
     <Card>
       <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Not connected to Captain server ({connection})
-        </Typography>
+        <Grid container direction="row" alignItems="center" spacing={1} style={{marginBottom: '0.8em'}}>
+          <Grid item>
+            <PortableWifiOffIcon color={"primary"} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h5" component="h2">
+              Not connected to Captain server ({connection})
+            </Typography>
+          </Grid>
+        </Grid>
         <Typography variant="body1">
           We were unable to connect to Captain server, there are a few reasons why this could be:
         </Typography>
