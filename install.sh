1c1c93613d12bed46a7a053163bddbe6776ad1d8
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2022-02-04 21:13:54 +0100

    Review setup to install dependencies in a virtualenv (#17)

--- install.sh
+++ install.sh
@@ -1,34 +1,54 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$EUID" -ne 0 ]
+if [[ "$EUID" -ne 0 ]]
   then echo "Please run as root: sudo make install"
   exit
 fi
 
-CAPTAIN_USER=captain
-getent group ${CAPTAIN_USER} >/dev/null 2>&1 || groupadd ${CAPTAIN_USER}
-id -u ${CAPTAIN_USER} >/dev/null 2>&1 || useradd -d /home/${CAPTAIN_USER} -g ${CAPTAIN_USER} -m ${CAPTAIN_USER}
+captain_user=captain
+echo "creating '${captain_user}' user (if needed)"
+getent group ${captain_user} >/dev/null 2>&1 || groupadd ${captain_user}
+id -u ${captain_user} >/dev/null 2>&1 || useradd -d /home/${captain_user} -g ${captain_user} -m ${captain_user}
 
-python3.9 setup.py build install
-mkdir -p /etc/captain/
-if [[ ! -f /etc/captain/config.yml ]]
+set +e
+echo "stopping running captain services (if running)"
+systemctl stop captain >/dev/null 2>&1
+set -e
+
+captain_dir=/usr/local/bin/captain
+echo "removing previous captain installation"
+rm -rf ${captain_dir}
+mkdir -p ${captain_dir}
+
+captain_server_dir="${captain_dir}/server"
+echo "installing captain server"
+mkdir -p ${captain_server_dir}
+python3.9 -m venv ${captain_server_dir}/venv/
+venv_python=${captain_server_dir}/venv/bin/python
+${venv_python} -m pip install --upgrade pip
+${venv_python} -m pip install -r requirements.txt
+${venv_python} setup.py build install
+
+captain_web_dir="${captain_dir}/web"
+echo "installing captain web application"
+mkdir -p ${captain_web_dir}
+cp -r web/src/ web/public/ web/.env web/package.json web/package-lock.json web/run-prod.sh ${captain_web_dir}/
+(cd ${captain_web_dir} && npm install)
+(cd ${captain_web_dir} && npm run build)
+
+captain_conf_dir=/etc/captain
+mkdir -p ${captain_conf_dir}
+if [[ ! -f "${captain_conf_dir}/config.yml" ]]
 then
-  cp captain/server/server-dev.yml /etc/captain/config.yml
+  echo "copying captain default configuration to ${captain_conf_dir}"
+  cp captain/server/server-dev.yml ${captain_conf_dir}/config.yml
 fi
-cp captain/server/captain-server.service /etc/systemd/system/
 
+cp captain/server/captain-server.service /etc/systemd/system/
 cp web/captain-web.service /etc/systemd/system/
-WEB_INSTALL_DIR=/usr/local/bin/captain/web
-mkdir -p ${WEB_INSTALL_DIR}
-cp -r web/* ${WEB_INSTALL_DIR}/
-cp -r web/.env ${WEB_INSTALL_DIR}/
-cd ${WEB_INSTALL_DIR}/
-npm install
-npm run build
+cp captain.service /etc/systemd/system
 
 systemctl daemon-reload
-systemctl enable captain-server
-systemctl restart captain-server
-systemctl enable captain-web
-systemctl restart captain-web
+systemctl enable captain captain-web captain-server
+systemctl start captain
